ac9d49b61707151a9f590821cccaac1f2c1a1612
Anthony <delantho327@gmail.com>
2022-01-23 03:18:18 -0600

    init commit

---
+++ Assets/JS/8BallMechanic.js
@@ -0,0 +1,38 @@
+let eightText = document.getElementById('8ballText');
+
+let getRandomInt = (7) => {
+  return Math.floor(Math.random() * max);
+}
+
+let eightBallQ = () =>{
+  switch (getRandomInt) {
+    case 1:
+    console.log("1")
+    eightText.innerHTML = "It will be so";
+    break;
+    case 2:
+    console.log("2")
+    eightText.innerHTML = "Possibly";
+    break;
+    case 3:
+    console.log("3")
+    eightText.innerHTML = "Maybe not";
+    break;
+    case 4:
+    console.log("4")
+    eightText.innerHTML = "No";
+    break;
+    case 5:
+    console.log("5")
+    eightText.innerHTML = "Yes";
+    break;
+    case 6:
+    console.log("6")
+    eightText.innerHTML = "Can Not say";
+    break;
+    case 7:
+    console.log("7")
+    eightText.innerHTML = "My vision is clouded";
+    break;
+  }
+}
